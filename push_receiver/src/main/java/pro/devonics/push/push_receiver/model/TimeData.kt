ca5c8fcb7bed9b2d4cdba49bdf4ec009dddb7edd
PushDevonics <91466356+PushDevonics@users.noreply.github.com>
2021-09-28 18:18:00 +0300

    Delete

--- push_receiver/src/main/java/pro/devonics/push/push_receiver/model/TimeData.kt
+++
@@ -1,9 +0,0 @@
-package pro.devonics.push.push_receiver.model
-
-import com.google.gson.annotations.SerializedName
-
-data class TimeData(
-    @SerializedName("registration_id")
-    private val registrationId: String,
-    private val time: Long
-)
